ba8efb8fae702a653ed4727a509212cd6c30ac4c
Paul-Philippe Wortmann <65237023+TottySnowman@users.noreply.github.com>
2024-09-15 19:55:29 +0200

    added tmux like shortcuts in wezterm

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -1,14 +1,148 @@
 local wezterm = require("wezterm")
 
-config = wezterm.config_builder()
+local config = wezterm.config_builder()
 
-config = {
-  automatically_reload_config = true,
-  enable_tab_bar = false,
-  window_decorations = "RESIZE",
-  color_scheme = "Catppuccin Macchiato",
-  default_cursor_style = "BlinkingBar",
-  font = wezterm.font("SauceCodePro Nerd Font", {weight="Black", stretch="Normal", style="Normal"}) -- (AKA: SauceCodePro NF, SauceCodePro NF Black)
+-- General settings
+config.automatically_reload_config = true
+config.hide_tab_bar_if_only_one_tab = false
+config.tab_bar_at_bottom = true
+config.use_fancy_tab_bar = false
+config.tab_and_split_indices_are_zero_based = true
+config.window_decorations = "RESIZE"
+config.color_scheme = "Catppuccin Macchiato"
+config.default_cursor_style = "BlinkingBar"
+config.font = wezterm.font("SauceCodePro Nerd Font", { weight = "Black", stretch = "Normal", style = "Normal" })
+
+-- tmux-like key bindings
+config.leader = { key = "q", mods = "ALT", timeout_milliseconds = 2000 }
+config.keys = {
+	{
+		mods = "LEADER",
+		key = "c",
+		action = wezterm.action.SpawnTab("CurrentPaneDomain"),
+	},
+	{
+		mods = "LEADER",
+		key = "x",
+		action = wezterm.action.CloseCurrentPane({ confirm = true }),
+	},
+	{
+		mods = "LEADER",
+		key = "b",
+		action = wezterm.action.ActivateTabRelative(-1),
+	},
+	{
+		mods = "LEADER",
+		key = "n",
+		action = wezterm.action.ActivateTabRelative(1),
+	},
+	{
+		mods = "LEADER",
+		key = "|",
+		action = wezterm.action.SplitHorizontal({ domain = "CurrentPaneDomain" }),
+	},
+	{
+		mods = "LEADER",
+		key = "-",
+		action = wezterm.action.SplitVertical({ domain = "CurrentPaneDomain" }),
+	},
+	{
+		mods = "LEADER",
+		key = "h",
+		action = wezterm.action.ActivatePaneDirection("Left"),
+	},
+	{
+		mods = "LEADER",
+		key = "j",
+		action = wezterm.action.ActivatePaneDirection("Down"),
+	},
+	{
+		mods = "LEADER",
+		key = "k",
+		action = wezterm.action.ActivatePaneDirection("Up"),
+	},
+	{
+		mods = "LEADER",
+		key = "l",
+		action = wezterm.action.ActivatePaneDirection("Right"),
+	},
+	{
+		mods = "LEADER",
+		key = "LeftArrow",
+		action = wezterm.action.AdjustPaneSize({ "Left", 5 }),
+	},
+	{
+		mods = "LEADER",
+		key = "RightArrow",
+		action = wezterm.action.AdjustPaneSize({ "Right", 5 }),
+	},
+	{
+		mods = "LEADER",
+		key = "DownArrow",
+		action = wezterm.action.AdjustPaneSize({ "Down", 5 }),
+	},
+	{
+		mods = "LEADER",
+		key = "UpArrow",
+		action = wezterm.action.AdjustPaneSize({ "Up", 5 }),
+	},
+	{
+		key = "l",
+		mods = "ALT",
+		action = wezterm.action.ShowLauncherArgs { flags = "FUZZY|WORKSPACES" },
+	},
+	{
+		key = "w",
+		mods = "ALT",
+		action = wezterm.action.PromptInputLine({
+			description = "Enter workspace name",
+			action = wezterm.action_callback(function(window, pane, line)
+				if line then
+					-- Switch to the workspace with the given name, or create it if it doesn't exist
+					window:perform_action(wezterm.action.SwitchToWorkspace({ name = line }), pane)
+				end
+			end),
+		}),
+	},
 }
+
+-- Add number key bindings for switching tabs
+for i = 0, 9 do
+	table.insert(config.keys, {
+		key = tostring(i),
+		mods = "LEADER",
+		action = wezterm.action.ActivateTab(i),
+	})
+end
+
+-- Status bar like tmux
+wezterm.on("update-right-status", function(window, _)
+	local SOLID_LEFT_ARROW = ""
+	local ARROW_FOREGROUND = { Foreground = { Color = "#c6a0f6" } }
+	local prefix = ""
+
+	if window:leader_is_active() then
+		prefix = " " .. utf8.char(0x1f30a) -- ocean wave
+		SOLID_LEFT_ARROW = utf8.char(0xe0b2)
+	end
+
+	if window:active_tab():tab_id() ~= 0 then
+		ARROW_FOREGROUND = { Foreground = { Color = "#1e2030" } }
+	end -- arrow color based on if tab is first pane
+
+	window:set_left_status(wezterm.format({
+		{ Background = { Color = "#b7bdf8" } },
+		{ Text = prefix },
+		ARROW_FOREGROUND,
+		{ Text = SOLID_LEFT_ARROW },
+	}))
+end)
+
+wezterm.on("update-status", function(window, pane)
+  local workspace = window:active_workspace()
+  window:set_right_status(wezterm.format({
+    {Text="Workspace: " .. workspace},
+  }))
+end)
 
 return config
