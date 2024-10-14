32fa7da6b0fe935f5f480ff13a23589dc6cf3e89
Paul-Philippe Wortmann <65237023+TottySnowman@users.noreply.github.com>
2024-10-14 22:46:14 +0200

    added display of hidden files

--- nvim/.config/nvim/lua/plugins/telescope.lua
+++ nvim/.config/nvim/lua/plugins/telescope.lua
@@ -1,35 +1,45 @@
 return {
-  {
-    "nvim-telescope/telescope.nvim",
-    tag = "0.1.6",
-    dependencies = { "nvim-lua/plenary.nvim" },
-    config = function()
-      local builtin = require("telescope.builtin")
-      vim.keymap.set("n", "<C-p>", builtin.find_files, {})
-      vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
-    end,
-  },
-  {
-    "nvim-telescope/telescope-ui-select.nvim",
-    'nvim-telescope/telescope-file-browser.nvim',
-    config = function()
-      require("telescope").setup({
-        extensions = {
-          ["ui-select"] = {
-            require("telescope.themes").get_dropdown({
-              -- even more opts
-            }),
-          },
-          ["file_browser"] = {
-            theme = "ivy",
-            hijack_netrw = true,
-          },
-        },
-      })
-      -- To get ui-select loaded and working with telescope, you need to call
-      -- load_extension, somewhere after setup function:
-      require("telescope").load_extension("ui-select")
-      require("telescope").load_extension("file_browser")
-    end,
-  },
+	{
+		"nvim-telescope/telescope.nvim",
+		tag = "0.1.6",
+		dependencies = { "nvim-lua/plenary.nvim" },
+		config = function()
+			local builtin = require("telescope.builtin")
+			vim.keymap.set("n", "<C-p>", builtin.find_files, {})
+			vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
+		end,
+	},
+	{
+		"nvim-telescope/telescope-ui-select.nvim",
+		"nvim-telescope/telescope-file-browser.nvim",
+		config = function()
+			require("telescope").setup({
+				defaults = {
+					file_ignore_patterns = { "node_modules" },
+					hidden = true,
+				},
+				pickers = {
+					find_files = {
+						hidden = true, -- Make sure `find_files` shows hidden files
+					},
+				},
+				extensions = {
+					["ui-select"] = {
+						require("telescope.themes").get_dropdown({
+							-- even more opts
+						}),
+					},
+					["file_browser"] = {
+						theme = "ivy",
+						hijack_netrw = true,
+						hidden = true,
+					},
+				},
+			})
+			-- To get ui-select loaded and working with telescope, you need to call
+			-- load_extension, somewhere after setup function:
+			require("telescope").load_extension("ui-select")
+			require("telescope").load_extension("file_browser")
+		end,
+	},
 }
