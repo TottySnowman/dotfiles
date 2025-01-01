7efe9629afa1fa10b9e1d5534cae1d843c4c0ef3
Paul-Philippe Wortmann <65237023+TottySnowman@users.noreply.github.com>
2025-01-01 23:13:51 +0100

    add vertical line after 150 chars

--- nvim/.config/nvim/lua/vim-options.lua
+++ nvim/.config/nvim/lua/vim-options.lua
@@ -3,6 +3,7 @@ vim.cmd("set tabstop=2")
 vim.cmd("set softtabstop=2")
 vim.cmd("set shiftwidth=2")
 vim.cmd("set relativenumber")
+vim.opt.colorcolumn = "150"
 -- Navigate vim panes better
 vim.keymap.set("n", "<c-k>", ":wincmd k<CR>")
 vim.keymap.set("n", "<c-j>", ":wincmd j<CR>")
